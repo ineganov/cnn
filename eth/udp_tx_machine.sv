52548625c9fc3e41b361d5357c9966e6440b28b9
root <root@debian>
2017-09-17 15:01:46 +0000

    UDP TX: IP checksum, packet count

--- eth/udp_tx_machine.sv
+++ eth/udp_tx_machine.sv
@@ -32,6 +32,10 @@ logic [15:0] my_port = 16'h4e50; // 20048
 
 logic [31:0] my_data;
 
+logic [15:0] pkt_count;
+logic [15:0] ip_chksum;
+logic [15:0] total_len;
+
 logic  [3:0] udp_state = 4'd1;
 logic  [3:0] udp_nextst;
 
@@ -76,19 +80,19 @@ always_comb
       11'd10: tx_data_n = my_hwaddr[1*8+:8];
       11'd11: tx_data_n = my_hwaddr[0*8+:8];
 
-      // ------------ IP Header ------------ //
-
       11'd12: tx_data_n = 8'h08; // is IP
       11'd13: tx_data_n = 8'h00; // is IP
+
+      // ------------ IP Header ------------ //
 
       11'd14: tx_data_n = 8'h45; // IPv4, 20 byte header
       11'd15: tx_data_n = 8'h00; // DSCP/ECN
 
-      11'd16: tx_data_n = 8'h00; // Total length
-      11'd17: tx_data_n = 8'h20; // Total length
+      11'd16: tx_data_n = total_len[1*8+:8]; // Total length
+      11'd17: tx_data_n = total_len[0*8+:8]; // Total length
 
-      11'd18: tx_data_n = 8'hAA; // ID
-      11'd19: tx_data_n = 8'hAA; // ID
+      11'd18: tx_data_n = pkt_count[1*8+:8]; // ID
+      11'd19: tx_data_n = pkt_count[0*8+:8]; // ID
 
       11'd20: tx_data_n = 8'h00; // Flags
       11'd21: tx_data_n = 8'h00; // Flags
@@ -96,8 +100,8 @@ always_comb
       11'd22: tx_data_n = 8'h40; // TTL
       11'd23: tx_data_n = 8'h11; // Proto UDP
 
-      11'd24: tx_data_n = 8'h4A; // IP Checksum
-      11'd25: tx_data_n = 8'hCB; // IP Checksum
+      11'd24: tx_data_n = ip_chksum[1*8+:8]; // IP Checksum
+      11'd25: tx_data_n = ip_chksum[0*8+:8]; // IP Checksum
 
       11'd26: tx_data_n = my_ip[3*8+:8]; // Src IP
       11'd27: tx_data_n = my_ip[2*8+:8]; // Src IP
@@ -118,7 +122,7 @@ always_comb
       11'd37: tx_data_n = my_port[0*8+:8]; // Dest port
 
       11'd38: tx_data_n = 8'h00; // UDP Length
-      11'd39: tx_data_n = 8'h0c; // UDP Length
+      11'd39: tx_data_n = 8'h1a; // UDP Length
 
       11'd40: tx_data_n = 8'h00; // UDP checksum
       11'd41: tx_data_n = 8'h00; // UDP checksum
@@ -137,8 +141,25 @@ always_ff@(posedge clk)
    if(tx_udp_dvld)
       my_data <= tx_udp_data;
 
+always_ff@(posedge clk)
+   if(reset)        pkt_count <= '0;
+   else if(tx_last) pkt_count <= pkt_count + 1'b1;
+
+assign total_len = 16'd46; // 20 bytes IP header + 26 UDP, incl header
+
+assign ip_chksum = ~(16'h0002        +
+                     16'h4500        +
+                     total_len       +
+                     pkt_count       +
+                     16'h0000        +
+                     16'h4011        +
+                     my_ip[1*16+:16] +
+                     my_ip[0*16+:16] +
+                     rx_ip[1*16+:16] +
+                     rx_ip[0*16+:16] );
+
 assign tx_req   = udp_state[UDP_ST_ARB];
-assign tx_count = {11{udp_state[UDP_ST_ARB]}} & 11'd60;
+assign tx_count = {11{udp_state[UDP_ST_ARB]}} & 11'd60; // 60 = 14 + 20 + 26 (Eth frame, IP hdr, UDP hdr+data)
 assign tx_data  = {8{udp_state[UDP_ST_HDR] | udp_state[UDP_ST_DATA]}} & tx_data_q;
 
 endmodule
